2b892793d88fd1d8de5a0417b618c3d3ea82621d
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2025-07-06 18:21:31 -0300

    fix logo button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import Logo from "../../../public/logo-2.png";
 import { Desktop } from "./desktop";
 import { Mobile } from "./mobile";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="flex bg-black items-center justify-between p-4 text-white border-b border-white fixed top-0 left-0 h-21 z-50 w-full ">
-      <Image src={Logo} width={80} height={80} alt="Logo" />
+      <Link href="/">
+        <Image src={Logo} width={80} height={80} alt="Logo" />
+      </Link>
 
       <Mobile />
       <Desktop />
